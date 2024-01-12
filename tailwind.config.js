63073bd31977cb330d783bc5667d1e081efde957
Richard Jérémy <jeremy.rc77@gmail.com>
2024-01-12 20:38:04 +0700

    css ok

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'anton' : ['Anton','sans-serif'],
+      'manrope' : ['Manrope','sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
