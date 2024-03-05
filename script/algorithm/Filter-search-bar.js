bdd802e1fb5432b3162b142f922ac464217a1472
Richard Jérémy <jeremy.rc77@gmail.com>
2024-03-05 16:48:19 +0100

    second branch ok

--- script/algorithm/Filter-search-bar.js
+++ script/algorithm/Filter-search-bar.js
@@ -1,19 +1,23 @@
 export const filterRecipes = (recipes, searchText) => {
-    // Converti le texte de recherche en minuscules pour une recherche insensible à la casse
-    const lowerCaseSearchText = searchText.toLowerCase();
-  
-    // Filtre les recettes
-    const filteredRecipes = recipes.filter(recipe => {
-      // Converti les champs de la recette en minuscules et les concaténer en une seule chaîne
-      const recipeText = (recipe.name + ' ' +
-                          recipe.ingredients.map(i => i.ingredient).join(' ') + ' ' +
-                          recipe.appliance + ' ' +
-                          recipe.ustensils.join(' ')).toLowerCase();
+  // Converti le texte de recherche en minuscules pour une recherche insensible à la casse
+  const lowerCaseSearchText = searchText.toLowerCase();
 
-  
-      // Vérifie si le texte de la recette contient le texte de recherche
-      return recipeText.includes(lowerCaseSearchText);
-    });
-  
-    return filteredRecipes;
+  // Filtre les recettes
+  let filteredRecipes = [];
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+
+    // Converti les champs de la recette en minuscules et les concaténer en une seule chaîne
+    let ingredientsText = '';
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      ingredientsText += recipe.ingredients[j].ingredient + ' ';
+    }
+
+    const recipeText = (recipe.name + ' ' +
+                        ingredientsText +
+                        recipe.appliance + ' ' +
+                        recipe.ustensils.join(' ')).toLowerCase();
   }
+
+  return filteredRecipes;
+}
